19e88b1b01dbdeee1c34bdf1f1623bcf6440a340
Gaganpreet Arora <me@gaganpreet.in>
2023-12-04 18:51:30 +0100

    Day 4 part 2

--- src/day4/main.rs
+++ src/day4/main.rs
@@ -30,11 +30,19 @@ fn main() {
 
     let regex = Regex::new(r"Card *(\d+): (.*) \| (.*)").unwrap();
     let mut points = 0;
+    let mut points_part_2 = 0;
     let power_base: i32 = 2;
 
-    for line in contents.lines() {
+    let mut extra_card_vector: Vec<u32> = Vec::with_capacity(contents.split("\n").count() - 1);
+
+    for i in 0..contents.split("\n").count() - 1 {
+        extra_card_vector.push(0);
+    }
+
+    for (i, line) in contents.lines().enumerate() {
         let captures = regex.captures(line).unwrap();
         let game = captures.get(1).unwrap().as_str();
+        let mut extra_points = 0;
         let winning_numbers_str = captures.get(2).unwrap().as_str();
         let have_numbers_str = captures.get(3).unwrap().as_str();
 
@@ -42,6 +50,7 @@ fn main() {
         let have_numbers_set = string_to_int_set(have_numbers_str);
 
         let have_count = have_numbers_set.intersection(&winning_numbers_set).count();
+        // println!("{:?}", extra_card_vector);
 
         /*
         println!(
@@ -53,9 +62,16 @@ fn main() {
             have_count,
         );
         */
+        for j in i + 1..i + 1 + have_count {
+            extra_card_vector[j] += 1 + extra_card_vector[i];
+        }
+
+        // println!("{}", extra_card_vector[i] + have_count as u32);
+
         if have_count > 0 {
             points += power_base.pow((have_count - 1) as u32);
         }
+        points_part_2 += extra_card_vector[i] + 1;
     }
-    println!("Points: {}", points);
+    println!("Points: {} {}", points, points_part_2);
 }
